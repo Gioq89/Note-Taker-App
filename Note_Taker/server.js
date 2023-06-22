ae0ffaf1f0e968bd877d17e1a227064372118715
giomendes.quezada <gioq89@gmail.com>
2023-06-21 17:25:16 -0700

    code to return saved notes as JSON

--- Note_Taker/server.js
+++ Note_Taker/server.js
@@ -3,9 +3,13 @@ const path = require('path');
 const fs = require('fs');
 const dataBase = require('./db/db.json');
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 const app = express();
+
+
+//HTML routes 
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -21,4 +25,17 @@ app.get('*', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Note taker listening at http://localhost:${PORT}`);
+  });
+
+  // Read the db.json file and return all saved notes as JSON
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to read notes data.' });
+      }
+  
+      const notes = JSON.parse(data);
+      res.json(notes);
+    });
   });
