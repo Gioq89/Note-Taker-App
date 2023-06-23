f00f1348212c23b2e92344e5c6df9fb13e20c48d
giomendes.quezada <gioq89@gmail.com>
2023-06-22 17:10:20 -0700

    fixed some bugs and added post functionality

--- Note_Taker/server.js
+++ Note_Taker/server.js
@@ -1,41 +1,64 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const dataBase = require('./db/db.json');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
-const PORT = process.env.port || 3001;
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-//HTML routes 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Note taker listening at http://localhost:${PORT}`);
+// API routes
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to read notes data." });
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
   });
+});
 
-  // Read the db.json file and return all saved notes as JSON
-app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'Failed to read notes data.' });
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to read notes data." });
+    }
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+
+    fs.writeFile(
+      path.join(__dirname, "db", "db.json"),
+      JSON.stringify(notes),
+      (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Failed to write note data." });
+        }
+        res.json(newNote);
       }
-  
-      const notes = JSON.parse(data);
-      res.json(notes);
-    });
+    );
   });
+});
+
+// HTML routes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+//Start the server
+app.listen(PORT, () => {
+  console.log(`Note taker listening at http://localhost:${PORT}`);
+});
