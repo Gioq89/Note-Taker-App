21312b1c50dfbf91c29f687ef9350c1c1a8840ea
giomendes.quezada <gioq89@gmail.com>
2023-06-22 17:43:21 -0700

    added delete request functionality

--- Note_Taker/server.js
+++ Note_Taker/server.js
@@ -49,6 +49,39 @@ app.post("/api/notes", (req, res) => {
   });
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to read notes data.' });
+    }
+
+    const notes = JSON.parse(data);
+
+    // Find the index of the note with the given id
+    const noteIndex = notes.findIndex((note) => note.id === noteId);
+
+    if (noteIndex === -1) {
+      return res.status(404).json({ error: 'Note not found.' });
+    }
+
+    // Remove the note from the array
+    notes.splice(noteIndex, 1);
+
+    // Write the updated notes array to the db.json file
+    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to write note data.' });
+      }
+
+      res.json({ message: 'Note deleted successfully.' });
+    });
+  });
+});
+
 // HTML routes
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
